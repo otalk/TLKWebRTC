1e45679cfc7ad8fddbfe0dae399df49059a186e4
Jon Hjelle <hjon@andyet.net>
2014-01-20 13:30:38 -0800

    Add JAHWebRTC files

---
+++ Classes/JAHWebRTC.h
@@ -0,0 +1,13 @@
+//
+//  JAHWebRTC.h
+//  
+//
+//  Created by Jon Hjelle on 1/20/14.
+//  Copyright (c) 2014 Jon Hjelle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JAHWebRTC : NSObject
+
+@end
---
+++ Classes/JAHWebRTC.m
@@ -0,0 +1,13 @@
+//
+//  JAHWebRTC.m
+//  
+//
+//  Created by Jon Hjelle on 1/20/14.
+//  Copyright (c) 2014 Jon Hjelle. All rights reserved.
+//
+
+#import "JAHWebRTC.h"
+
+@implementation JAHWebRTC
+
+@end
