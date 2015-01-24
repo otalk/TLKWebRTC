360aacf916034755f395a1cf64570b7bc8b37293
Patrick Piemonte <piemonte@alumni.cmu.edu>
2015-01-23 17:32:19 -0800

    TLKWebRTC, use declaration code style of UIKit pointers

--- Classes/TLKWebRTC.h
+++ Classes/TLKWebRTC.h
@@ -20,25 +20,25 @@
 
 - (instancetype)initWithVideo:(BOOL)allowVideo;
 
-- (void)addPeerConnectionForID:(NSString*)identifier;
-- (void)removePeerConnectionForID:(NSString*)identifier;
+- (void)addPeerConnectionForID:(NSString *)identifier;
+- (void)removePeerConnectionForID:(NSString *)identifier;
 
-- (void)createOfferForPeerWithID:(NSString*)peerID;
-- (void)setRemoteDescription:(RTCSessionDescription*)remoteSDP forPeerWithID:(NSString*)peerID receiver:(BOOL)isReceiver;
-- (void)addICECandidate:(RTCICECandidate*)candidate forPeerWithID:(NSString*)peerID;
+- (void)createOfferForPeerWithID:(NSString *)peerID;
+- (void)setRemoteDescription:(RTCSessionDescription *)remoteSDP forPeerWithID:(NSString *)peerID receiver:(BOOL)isReceiver;
+- (void)addICECandidate:(RTCICECandidate *)candidate forPeerWithID:(NSString *)peerID;
 
 // Add a STUN or TURN server, adding a STUN server replaces the previous STUN server, adding a TURN server appends it to the list
-- (void)addICEServer:(RTCICEServer*)server;
+- (void)addICEServer:(RTCICEServer *)server;
 
 // The WebRTC stream captured locally that will be sent to peers, useful for displaying a preview of the local camera
 // in an RTCVideoRenderer and muting or blacking out th stream sent to peers
-@property (readonly, nonatomic) RTCMediaStream* localMediaStream;
+@property (readonly, nonatomic) RTCMediaStream *localMediaStream;
 
 @end
 
 // WebRTC signal delegate protocol
 @protocol TLKWebRTCDelegate <NSObject>
-@optional
+@required
 - (void)webRTC:(TLKWebRTC *)webRTC didSendSDPOffer:(RTCSessionDescription *)offer forPeerWithID:(NSString *)peerID;
 - (void)webRTC:(TLKWebRTC *)webRTC didSendSDPAnswer:(RTCSessionDescription *)answer forPeerWithID:(NSString* )peerID;
 - (void)webRTC:(TLKWebRTC *)webRTC didSendICECandidate:(RTCICECandidate *)candidate forPeerWithID:(NSString *)peerID;
